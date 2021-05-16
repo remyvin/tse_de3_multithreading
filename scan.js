736666f0eb2dbb1693d538c110cb7dbfad7282c3
DarKoX42 <34058002+DarKoX42@users.noreply.github.com>
2021-05-16 03:45:22 +0200

    Ajout de commentaires

--- scan.js
+++ scan.js
@@ -11,21 +11,22 @@ const r1 = readline.createInterface({
 	output: process.stdout
 });
 
+/* Worker Thread */
 function fromDir(startPath,filter){
 	var list = [];
     if (!fs.existsSync(startPath)){
-        console.log("no dir ",startPath);
+        console.log("no dir ",startPath); /* Le dossier n'existe pas */
         return;
     }
 
     var files=fs.readdirSync(startPath);
-	fs.createReadStream('list.csv')
+	fs.createReadStream('list.csv') /* Permet de lire le fichier csv */
 	.pipe(csv({}))
 	.on('data', (data) => results.push(data))
 	.on('end', () => {
 		console.log(files);
 		file_for_sloop:
-		for(var i=0;i<files.length;i++){
+		for(var i=0;i<files.length;i++){ /* Itere sur les differents fichiers du dossier */
 			var filename=path.join(startPath,files[i]);
 			var stat = fs.lstatSync(filename);
 			if (stat.isDirectory()){
@@ -36,16 +37,16 @@ function fromDir(startPath,filter){
 					var options = JSON.parse(results[j].options);
 					if (options.image == filename)
 					{
-						continue file_for_sloop;
+						continue file_for_sloop; /* Petit continue sur plusieurs boucles via un label */
 					}
 				}
 				(async function () { // asynchrone 
 					try {
 						// Get file name
 						const words = filename.split('\\'); // find last Index Of \
-						var path_out = folder_out.concat(words[1]);
+						var path_out = folder_out.concat(words[1]); /* Concatene le nom du fichier au nom de dossier */
 						// Resizes the image
-						sharp(filename).resize(50, 50).png().toFile(path_out);
+						sharp(filename).resize(50, 50).png().toFile(path_out); /* Resize l'image et l'envoi dans le bon dossier */
 					} catch (error) {
 						console.log(error);
 					}
@@ -58,7 +59,7 @@ function fromDir(startPath,filter){
 					list.push(metadata);	
 					if(i==files.length - 1){	
 						const csv = new ObjectsToCsv(list);
-						csv.toDisk('./list.csv', { append: true });
+						csv.toDisk('./list.csv', { append: true }); /* Ecrit sur le fichier CSV */
 					}
 				} catch (error) {
 					console.log('Error: ' + error.message);
@@ -68,52 +69,63 @@ function fromDir(startPath,filter){
 	});
 };
 
+/* Main thread */
 async function main(){
 	const results_output = [];
-	var clef = await ask("Hello you can add a word and we will see what we can find for you mate ? ")
+	var clef = await ask("Hello put a keyword :)")
 	fs.createReadStream('list.csv')
 	.pipe(csv({}))
 	.on('data', (data) => results_output.push(data))
-	.on('end', async(recommencer) => {
-		for(var j=0;j<results_output.length;j++){
+	.on('end', async(recommencer) => { /* Sans le async on ne peut pas appeler la fonction ask */
+		for(var j=0;j<results_output.length;j++){ /* Itere sur les images en base */
 			var image_path;
 			var display = 0;
-			JSON.parse(results_output[j].options, (key, value) => {
+			JSON.parse(results_output[j].options, (key, value) => { /* Permet de parser les metadatas */
 				if (key == "image")
 				{
-					image_path = value;
+					image_path = value; /* Recupere la valeur du chemin de l'image */
 				}
 				if (value == clef){
 					if (display == 0)
 					{
 						console.log(image_path);
-						display = 1;
+						display = 1; 
+						/* Si la recherche trouve une correspondre et affiche l'image on passe à celle d'après */
+						/* Sans cette dernière on afficherait plusieurs fois la même image */
 					}
 				}
 			});
 		}
+		/* Appel de la fonction qui permet de saisir un utilisateur */
 		recommencer = await ask("Do you want to continue ? If Yes, Press y : ")
 		if( recommencer == 'y')
 		{
+			/* Permet de relancer la fonction si l'utilisateur le souhaite */
 			setTimeout(main, 1000);	
 		}
 		else 
 		{
+			/* Fin du programme */
 			process.exit()
 		}
 	});
 };
+
+/* Fonction qui va permettre de demander à l'utilisateur de saisir un key word pour la recherche */
 function ask(questionText) {
 	return new Promise((resolve, reject) => {
 	  r1.question(questionText, resolve);
 	});
   }
-//main()
+
+/* Fonction qui va permettre d'appeler les trois workers toutes les 5 secondes */
 function FromDirs(){
 	fromDir('./image','.jpg');
 	fromDir('./image1','.jpg');
 	fromDir('./image2','.jpg');
 	setTimeout(FromDirs, 5000); // appel des 3 dossiers toutes les secondes task va appeler fromdir 3 fois
 }
+
+/* Appel des fonctions */
 main();
 FromDirs();
